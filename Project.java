64d46857beb3bda6ce97863b9cade84a77bf8f56
Niranjana Moothandassery <niranjana.moothandassery@msds.christuniversity.in>
2023-05-02 12:25:18 +0530

    project changes

--- Project.java
+++ Project.java
@@ -4,7 +4,7 @@ public class Project{
         System.out.println("Hello, World!");
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/event_planning","root","Niranjana@mysql");
+            Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/event_planning","root","mysql1234");
             Statement statement = connection.createStatement();
             ResultSet resultSet=statement.executeQuery("SELECT * FROM caterer");
             while (resultSet.next()) {
